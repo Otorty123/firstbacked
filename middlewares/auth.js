f31d1125766824a9a29050a24d3c4e3596c5c11c
unknown <alamoyoolayinka@gmail.com>
2023-11-21 14:20:32 +0000

    us

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,1 +1,9 @@
-let mongoose;
+const isLoggedin = async(req, res, next)=>{
+    if(req.is) return next()
+    return res.json({error: "Login session is expired"})
+}
+
+const isSubscribed = async(req, res, next)=>{
+    if(req.isAuthenticated () && req.user) return next()
+    return res.json({error:"Subscription is expired"})
+};
