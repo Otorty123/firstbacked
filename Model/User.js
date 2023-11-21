4b9060adb814ba34dcc4546eddc9d89e891630ef
HP-PC <sordyq4242@gmail.com>
2023-11-21 13:09:56 +0100

    usermodel

--- Model/User.js
+++ Model/User.js
@@ -1,1 +1,42 @@
-let mongoose;
+const mongoose = require("mongoose");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose")
+const{Schema, model} = mongoose;
+const userSchema = new Schema({
+    username:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    isAdmin:{
+        type:Boolean,
+        default:false
+    },
+    rquestCount:{
+        type:Number,
+        default:0
+    },
+    subscription:{
+        type:String,
+        enum:["free","basic","premium"],
+        default:"free"
+    }
+
+})
+
+userSchema.plugin(passportLocalMongoose)
+
+passport.use(Usermodel.createStrategy())
+
+passport.serializeUser(Usermodel.serializeUser())
+
+passport.deserializeUser(Usermodel.deserializeUser())
+
+module.express = Usermodel
